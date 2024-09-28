28dcf4ff8d79ca95092c60f4e65704bf62a386ba
kx9 <kuangxq2022@163.com>
2024-09-28 17:56:33 +0800

    节点可用性

--- src/main.js
+++ src/main.js
@@ -4,17 +4,16 @@ import router from "./router/index";
 import App from './App.vue'
 import axios from 'axios'
 
-// axios.defaults.baseURL='http://10.96.208.18:8080/'
-//axios.defaults.baseURL='http://192.168.181.1:8080/'       //lin
-//axios.defaults.baseURL='http://10.100.4.214:8080/'       //lin
-//axios.defaults.baseURL='http://10.96.228.235:9090/'
+
+
 
 
 // axios.defaults.baseURL='http://124.223.171.19:9090/' //A可用
 axios.defaults.baseURL='http://101.43.94.172:9091/' //B可用
+// axios.defaults.baseURL='http://124.222.196.78:9091/' //E可用
 // axios.defaults.baseURL='http://124.221.254.11:8080/' //C
 // axios.defaults.baseURL='http://124.223.210.53:8080/' //D
-// axios.defaults.baseURL='http://124.222.196.78:9091/' //E可用
+//axios.defaults.baseURL='http://10.96.228.235:9090/' //F
 
 const app=createApp(App)
 app.provide('$axios', axios)
